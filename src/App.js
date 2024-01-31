6a8bd636af0ad4e6777c8e217847319123ac7674
Ahmed Raza <bhaia9036@gmail.com>
2024-01-31 14:00:12 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
+import Home from "./Pages/Home/Home";
 import logo from "./logo.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Components/Navbar/Home/Home";
-import FAQ from "../src/Pages/FAQ/FAQ";
-import Testimonials from "../src/Pages/Testimonials/Testimonials";
-import Careers from "../src/Pages/Careers/Careers";
-import Signup from "./Pages/Siginup/Signup";
-import Login from "./Pages/Login/Login";
-import Howwork from "./Pages/Howworks";
-import ABoutus from "./Pages/Aboutus/ABoutus";
-import BuyBasic from "./Pages/Buypages/BuyBasic";
-import BuyAmateur from "./Pages/Buypages/BuyAmateur";
-import BuyProfessional from "./Pages/Buypages/Buyprofessional";
-import Leaderboard from "./Pages/Clientarea/Leaderboard";
-import Addimage from "./Pages/Addimage/Addimage";
-import Addsignal from "./Pages/forexsignalgroup/Addsignalgroup/Addsignal";
-import SignalGroup from "./Pages/forexsignalgroup/SignalGroup";
-import Manager from "./Pages/AccountManager/Manager";
-import Addmanager from "./Pages/AccountManager/Addmanager/Addmanager";
-import Mentorships from "./Pages/Mentorships/Mentorship";
-import Addmentors from "./Pages/Mentorships/Addmentors/AddMentors";
-import Success from "./Pages/Afterpayment/Success";
-import Cancel from "./Pages/Afterpayment/Cancel";
 
 function App() {
   return (
@@ -28,25 +8,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path={"/"} element={<Home />} />
-          <Route path={"/FAQ"} element={<FAQ />} />
-          <Route path={"/Testimonials"} element={<Testimonials />} />
-          <Route path={"/Signup"} element={<Signup />} />
-          <Route path={"/login"} element={<Login />} />
-          <Route path={"/Howworks"} element={<Howwork />} />
-          <Route path={"/Aboutus"} element={<ABoutus />} />
-          <Route path={"/BuyBasic"} element={<BuyBasic />} />
-          <Route path={"/BuyAmateur"} element={<BuyAmateur />} />
-          <Route path={"/BuyProfessional"} element={<BuyProfessional />} />
-          <Route path={"/Clientarea"} element={<Leaderboard />} />
-          <Route path={"/Addimage"} element={<Addimage />} />
-          <Route path={"/Addsignalgroup"} element={<Addsignal />} />
-          <Route path={"/signalgroup"} element={<SignalGroup />} />
-          <Route path={"/accountmanager"} element={<Manager />} />
-          <Route path={"/Addaccountmanager"} element={<Addmanager />} />
-          <Route path={"/mentorship"} element={<Mentorships />} />
-          <Route path={"/addmentors"} element={<Addmentors />} />
-          <Route path={"/success"} element={<Success />} />
-          <Route path={"/cancel"} element={<Cancel />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  const containerStyle = {
+ // Adjust the width as needed
+    Animation:""
+    // background: 'url("https://www.osimo.com.tr/assets/images/media-bg.jpg") center/cover no-repeat',
+  };
+  return (
+    <div>
+        Hello
+    </div>
+  )
+}
+
+export default Home
