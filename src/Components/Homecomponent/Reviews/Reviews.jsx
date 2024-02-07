144a4f8b64d3fb72cf630b2ee15a914fdcd6ebc8
Ahmed Raza <bhaia9036@gmail.com>
2024-02-07 18:18:14 +0500

    first commit

--- src/Components/Homecomponent/Reviews/Reviews.jsx
+++ src/Components/Homecomponent/Reviews/Reviews.jsx
@@ -17,13 +17,7 @@ const Reviews = () => {
 
   return (
     <div>
-        <div className='ml-4 md:ml-16 p-4 mb-8'>
-  <div style={{ "textTransform": "uppercase" }} className='text-3xl md:text-3xl font-bold mt-6'>Testimonials </div>
 
-  <div className='w-full md:w-72 border-2 border-amber-400' >
-    {/* Content for the inner div */}
-  </div>
-</div>
 <div className='mb-6'>
 
 <div class="elfsight-app-202fc3b5-10f7-4422-aa76-21727e037f91" data-elfsight-app-lazy></div>
